6579ae0fb8807abbac605fc9381062cce56c0354
VaibhavDhiman1 <vaibhavdhiman143@gmail.com>
2026-05-11 22:48:24 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,17 +3,19 @@ import {
   FaFolderOpen,
   FaTasks,
   FaSignOutAlt,
+  FaTimes,
 } from "react-icons/fa";
 
 import { Link, useNavigate } from "react-router-dom";
 
 import toast from "react-hot-toast";
 
-const Sidebar = () => {
+const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   const navigate = useNavigate();
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
+
     localStorage.removeItem("user");
 
     toast.success("Logged out successfully");
@@ -22,23 +24,38 @@ const Sidebar = () => {
   };
 
   return (
-    <aside className="fixed md:static z-50 w-64 bg-white shadow-lg flex-col justify-between">
-      
-      <div>
-        <div className="p-6 border-b">
-          <h1 className="text-2xl font-bold text-indigo-600">
-            TaskFlow
-          </h1>
+    <aside
+      className={`
+fixed top-0 left-0 h-screen bg-white shadow-lg z-50
+w-64 flex flex-col justify-between
+transition-transform duration-300
 
-          <p className="text-sm text-gray-500 mt-1">
-            Team Workspace
-          </p>
+${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
+
+lg:translate-x-0 lg:static
+`}
+    >
+      <div>
+        {/* Header */}
+        <div className="p-6 border-b relative">
+          <h1 className="text-2xl font-bold text-indigo-600">TaskFlow</h1>
+
+          <p className="text-sm text-gray-500 mt-1">Team Workspace</p>
+
+          {/* Close Button */}
+          <button
+            onClick={() => setSidebarOpen(false)}
+            className="lg:hidden absolute top-6 right-5 text-xl text-gray-600"
+          >
+            <FaTimes />
+          </button>
         </div>
 
+        {/* Navigation */}
         <nav className="p-4 space-y-2">
-          
           <Link
             to="/"
+            onClick={() => setSidebarOpen(false)}
             className="flex items-center gap-3 p-3 rounded-xl hover:bg-indigo-50 text-gray-700 transition"
           >
             <FaHome />
@@ -47,6 +64,7 @@ const Sidebar = () => {
 
           <Link
             to="/projects"
+            onClick={() => setSidebarOpen(false)}
             className="flex items-center gap-3 p-3 rounded-xl hover:bg-indigo-50 text-gray-700 transition"
           >
             <FaFolderOpen />
@@ -55,15 +73,16 @@ const Sidebar = () => {
 
           <Link
             to="/tasks"
+            onClick={() => setSidebarOpen(false)}
             className="flex items-center gap-3 p-3 rounded-xl hover:bg-indigo-50 text-gray-700 transition"
           >
             <FaTasks />
             Tasks
           </Link>
-
         </nav>
       </div>
 
+      {/* Logout */}
       <div className="p-4">
         <button
           onClick={logoutHandler}
