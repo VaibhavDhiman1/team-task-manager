a3aaa16fe7b0f98604b6f1e4f523616546117713
VaibhavDhiman1 <vaibhavdhiman143@gmail.com>
2026-05-11 22:48:52 +0530

    Update DashboardLayout.jsx

--- client/src/layout/DashboardLayout.jsx
+++ client/src/layout/DashboardLayout.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
+
+import { FaBars } from "react-icons/fa";
+
 import Sidebar from "../components/Sidebar";
+
 import Navbar from "../components/Navbar";
 
 const DashboardLayout = ({ children }) => {
-  return (
-    <div className="min-h-screen md:flex bg-gray-100">
-      
-      <Sidebar />
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  return (
+    <div className="min-h-screen flex bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* Overlay */}
+      {sidebarOpen && (
+        <div
+          onClick={() => setSidebarOpen(false)}
+          className="fixed inset-0 bg-black/40 z-40 lg:hidden"
+        />
+      )}
+
+      {/* Main Content */}
       <div className="flex-1 flex flex-col">
-        
+        {/* Mobile Header */}
+        <div className="lg:hidden bg-white shadow-sm p-4 flex items-center">
+          <button
+            onClick={() => setSidebarOpen(true)}
+            className="text-2xl text-gray-700"
+          >
+            <FaBars />
+          </button>
+        </div>
+
         <Navbar />
 
-        <main className="p-6">
-          {children}
-        </main>
-
+        <main className="p-6">{children}</main>
       </div>
     </div>
   );
