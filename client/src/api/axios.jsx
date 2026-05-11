46f06869ad375a4c4b1c9b3d2fb6a3bbd1622122
VaibhavDhiman1 <vaibhavdhiman143@gmail.com>
2026-05-11 17:00:15 +0530

    Update axios.jsx

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://team-task-manager-production-2949.up.railway.app/",
 });
 
 API.interceptors.request.use((config) => {
